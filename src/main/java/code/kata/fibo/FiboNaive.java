4952b573559f1ed3bd8334ebc60a60291d37d354
Simone Bächle <simone.baechle@senacor.com>
2020-07-11 13:31:02 +0200

    Implementation of FiboNaive

--- src/main/java/code/kata/fibo/FiboNaive.java
+++ src/main/java/code/kata/fibo/FiboNaive.java
@@ -3,7 +3,10 @@ package code.kata.fibo;
 public class FiboNaive {
 
     public int fibonacci(int index) {
-        return 0;
-    }
+        if(index == 0 || index == 1) {
+            return 1;
+        }
 
+        return fibonacci(index - 1) + fibonacci(index - 2);
+    }
 }
