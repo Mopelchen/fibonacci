755ae8b58a08ea737d946f4adc90982095e32aba
Simone Bächle <simone.baechle@senacor.com>
2020-07-11 13:47:02 +0200

    Measuring time elapsed

--- src/main/java/code/kata/fibo/FiboNaive.java
+++ src/main/java/code/kata/fibo/FiboNaive.java
@@ -1,12 +1,24 @@
 package code.kata.fibo;
 
+import static java.lang.String.format;
+
 public class FiboNaive {
 
     public int fibonacci(int index) {
+
+        long startTime = System.nanoTime();
+        int fibo = compute(index);
+        long endTime = System.nanoTime();
+        System.out.println(format("Computation of fibonacci(%d) took %d ns", index, (endTime - startTime)));
+
+        return fibo;
+    }
+
+    private int compute(int index) {
         if(index == 0 || index == 1) {
             return 1;
         }
 
-        return fibonacci(index - 1) + fibonacci(index - 2);
+        return compute(index - 1) + compute(index - 2);
     }
 }
